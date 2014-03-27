a7173c335573c6624bdecf26d525eb5d7c89b161
dotherightthing <dan.smith@chrometoaster.com>
2014-03-27 20:23:06 +1300

    Merge branch 'master' of https://github.com/chrometoasters/ct-grunt-styleguide
    Conflicts:
    grunt-tasks/options/string-replace.js
    package.json

---
+++ tests/test-1/resources/styleguide-page.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Test styleguide</title>
+        <link rel="stylesheet" href="/test-1/resources/css/styles.css" />
+        <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+    </head>
+    <body>
+        <?php
+            // START KSS STYLEGUIDE
+            include( $_SERVER['DOCUMENT_ROOT'] . '/test-1/resources/styleguide/index.php' );
+            // END KSS STYLEGUIDE
+        ?>
+    </body>
+</html>
